147655bf2dfec03f020fe47c01c19ed4022fd943
Simon Kamangaru <63295027+SimonMunyiri@users.noreply.github.com>
2024-09-15 06:47:42 +0300

    Add files via upload

---
+++ python.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#@author Simon kamangaru
+#bash script automation
+
+#navigate to the folder that contains the '.sh. file
+cd /home/kamangaru/Desktop/pycharm-community-2021.2.3/bin
+
+#run the pycharm script to run the software
+./pycharm.sh
